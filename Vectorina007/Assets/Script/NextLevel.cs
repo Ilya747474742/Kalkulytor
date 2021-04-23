ded4ec4fe83df28a8c2dead72d36c15c97703b34
Ilya747474742 <68121715+Ilya747474742@users.noreply.github.com>
2021-04-23 23:41:14 +0700

    152

---
+++ Vectorina007/Assets/Script/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class NextLevel : MonoBehaviour
+{
+    public int buildindex;
+    public void Levelss()
+    {
+        SceneManager.LoadScene(buildindex);
+
+    }
+}
