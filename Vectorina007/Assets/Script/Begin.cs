5b751ee8115da03fe5b73514530c09a93c04e09c
Ilya747474742 <68121715+Ilya747474742@users.noreply.github.com>
2021-04-26 23:25:09 +0700

    12

--- Vectorina007/Assets/Script/Begin.cs
+++ Vectorina007/Assets/Script/Begin.cs
@@ -5,25 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Begin : MonoBehaviour
 {
-    public InputField Name;
+
     public int buildindex;
     public void Levelss()
     {
         SceneManager.LoadScene(buildindex);
 
 
-        try
-        {
-
-            string a = (Name.text);
-            print(a);
-
-
-        }
-        catch (Exception)
-        {
-            Name.text = "Wrong Value";
-
-        }
     }
 }
